f5ae47488bd2524ce7cf111a6bb5d9bfe42ef052
chroosh <christopher.shi22@gmail.com>
2020-04-05 17:33:49 +1000

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# chroosh.github.io
---
+++ menubar.js
@@ -0,0 +1,8 @@
+document.getElementById("menubar").innerHTML =
+'<ul class="nav">'+
+    '<li><a href="index.html">Home</a></li>' + 
+    '<li><a href="blogs.html">Blogs</a></li>' + 
+    '<li><a href="projects.html">Projects</a></li>' + 
+    '<li><a href="videos.html">Videos</a></li>' +
+    '<li><a href="contact.html">Contact</a></li>' +
+'</ul>';
