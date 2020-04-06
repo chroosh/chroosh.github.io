a1215178608c0c714fa8acf291076d0eebb692fc
chroosh <christopher.shi22@gmail.com>
2020-04-06 17:19:21 +1000

    overhauled menubar and using css grid to layout content

--- menubar.js
+++ menubar.js
@@ -1,8 +1,8 @@
 document.getElementById("menubar").innerHTML =
 '<ul class="nav">'+
-    '<li><a href="index.html">Home</a></li>' + 
-    '<li><a href="blogs.html">Blogs</a></li>' + 
-    '<li><a href="projects.html">Projects</a></li>' + 
-    '<li><a href="videos.html">Videos</a></li>' +
+    '<li class="home"><a href="index.html"><u>Hello<u></a></li>' + 
     '<li><a href="contact.html">Contact</a></li>' +
+    '<li><a href="videos.html">Videos</a></li>' +
+    '<li><a href="projects.html">Projects</a></li>' + 
+    '<li><a href="blogs.html">Blogs</a></li>' + 
 '</ul>';
